4849eff5c89d45423d17df4b69fce54033301d69
shiraidris007 <shiraidris007@gmail.com>
2024-06-04 15:54:44 -0700

    scrape other counties

---
+++ County-SO-Data/Grays Harbor/scrape_graysharbor.R
@@ -0,0 +1,30 @@
+library(googlesheets4) # only needed if data are stored in a googlesheet
+library(googledrive)
+library(readxl)
+
+# Examples to see if you can find/specify a unique folder and filename
+# See: https://googledrive.tidyverse.org/reference/drive_find.html
+# The googledrive functions should pop up an authorization dialog in your
+# web-browser if necessary
+
+#drive_find("Asotin County", type="folder")
+#drive_get("Asotin County/Asotin.xlsx")
+
+# Download an excel file from the drive
+dl <- drive_download(file = "Grays Harbor County/GraysHarbor.xlsx", # google drive filename
+                     path = "County-SO-Data/Grays Harbor/GraysHarbor.xlsx", # local filename
+                     overwrite = TRUE, 
+                     type = "xlsx")
+
+# Read data from the downloaded file
+# KC gave us incident-based data, so we have both that and the pivot table
+
+# have not got this far yet for asotin king.inci <- read_xlsx("County-SO-Data/King/King.xlsx")
+
+#king.tbl <- read_xlsx(
+#  "County-SO-Data/King/King.xlsx",
+#  sheet = "Table",
+#  skip = 2
+#)
+
+save.image(file="County-SO-Data/King/King.rda")
