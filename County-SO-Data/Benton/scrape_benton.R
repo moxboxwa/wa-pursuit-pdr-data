f02b3d37e11658f923454c7ade7a23fa15375479
shiraidris007 <shiraidris007@gmail.com>
2024-08-14 11:53:03 -0700

    benton upload

---
+++ County-SO-Data/Benton/scrape_benton.R
@@ -0,0 +1,30 @@
+library(googlesheets4) # only needed if data are stored in a googlesheet
+library(googledrive)
+library(readxl)
+
+# Examples to see if you can find/specify a unique folder and filename
+# See: https://googledrive.tidyverse.org/reference/drive_find.html
+# The googledrive functions should pop up an authorization dialog in your
+# web-browser if necessary
+
+drive_find("Benton County", type="folder")
+drive_get("Benton County/Benton.xlsx")
+
+# Download an excel file from the drive
+dl <- drive_download(file = "Benton County/Benton.xlsx", # google drive filename
+                     path = "County-SO-Data/Benton/Benton.xlsx", # local filename
+                     overwrite = TRUE, 
+                     type = "xlsx")
+
+# Read data from the downloaded file
+# KC gave us incident-based data, so we have both that and the pivot table
+
+benton.inci <- read_xlsx("County-SO-Data/Benton/Benton.xlsx")
+
+benton.tbl <- read_xlsx(
+  "County-SO-Data/Benton/Benton.xlsx",
+  sheet = "Table",
+  skip = 2
+)
+
+save.image(file="County-SO-Data/Benton/Benton.rda")
