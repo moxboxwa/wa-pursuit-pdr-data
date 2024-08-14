eb5887fb70a9b520d9d8d89b14dfadaae9872a15
shiraidris007 <shiraidris007@gmail.com>
2024-08-14 13:02:32 -0700

    more updates

---
+++ County-SO-Data/Grant/scrape_grant.R
@@ -0,0 +1,30 @@
+library(googlesheets4) # only needed if data are stored in a googlesheet
+library(googledrive)
+library(readxl)
+
+# Examples to see if you can find/specify a unique folder and filename
+# See: https://googledrive.tidyverse.org/reference/drive_find.html
+# The googledrive functions should pop up an authorization dialog in your
+# web-browser if necessary
+
+#drive_find("Grant County", type="folder")
+#drive_get("Grant County/Benton.xlsx")
+
+# Download an excel file from the drive
+dl <- drive_download(file = "Grant County/Grant.xlsx", # google drive filename
+                     path = "County-SO-Data/Grant/Grant.xlsx", # local filename
+                     overwrite = TRUE, 
+                     type = "xlsx")
+
+# Read data from the downloaded file
+
+#grant has no inci level
+#grant.inci <- read_xlsx("County-SO-Data/Grant/Grant.xslx")
+
+grant.tbl <- read_xlsx(
+  "County-SO-Data/Grant/Grant.xslx",
+  sheet = "Table",
+  skip = 2
+)
+
+save.image(file="County-SO-Data/Grant/Grant.rda")
