7c9a38058145892651b33916c92298f3526b25de
shiraidris007 <shiraidris007@gmail.com>
2024-09-04 14:29:33 -0700

    island

---
+++ County-SO-Data/Island/scrape_island.R
@@ -0,0 +1,19 @@
+library(googlesheets4) # only needed if data are stored in a googlesheet
+library(googledrive)
+library(readxl)
+
+# Examples to see if you can find/specify a unique folder and filename
+# See: https://googledrive.tidyverse.org/reference/drive_find.html
+# The googledrive functions should pop up an authorization dialog in your
+# web-browser if necessary
+
+
+# Read data from the downloaded file
+# no inci level data
+
+island.tbl <- read_xlsx(
+  "County-SO-Data/Island/Island.xlsx",
+  skip = 2
+)
+
+save.image(file="County-SO-Data/Island/Island.rda")
