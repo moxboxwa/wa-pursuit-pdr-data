d2230b3749b5082036cf33036ac8af1c043e96c9
Martina Morris <morrism@uw.edu>
2024-10-09 15:26:11 -0700

    more changes to asotin

--- County-SO-Data/Asotin/scrape_asotin.R
+++ County-SO-Data/Asotin/scrape_asotin.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 library(googlesheets4) # only needed if data are stored in a googlesheet
 library(googledrive)
 library(readxl)
@@ -19,13 +20,13 @@ dl <- drive_download(file = "Asotin County/Asotin.xlsx", # google drive filename
 # Read data from the downloaded file
 # KC gave us incident-based data, so we have both that and the pivot table
 
-asotin.inci <- read_xlsx("County-SO-Data/Asotin/Asotin.xlsx")
+asotin.inci <- read_xlsx("County-SO-Data/Asotin/Asotin.xlsx",
+                         skip = 1)
 
-asotin.tbl <- read_xlsx(
-  "County-SO-Data/Asotin/Asotin.xlsx",
-  sheet = "Table",
-  skip = 2
-)
+asotin.tbl <- read_excel("County-SO-Data/Asotin/Asotin.xlsx", 
+                         sheet = "Table", 
+                         skip = 3, 
+                         n_max = 5)
 
 rm("dl")
 save.image(file="County-SO-Data/Asotin/Asotin.rda")
