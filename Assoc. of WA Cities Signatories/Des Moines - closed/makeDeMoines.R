ee7bc13c1f044b8806e8a65e81e6c54b56fea6b6
Martina Morris <morrism@uw.edu>
2025-03-05 17:23:17 -0800

    Des Moines data harmonization

---
+++ Assoc. of WA Cities Signatories/Des Moines - closed/makeDeMoines.R
@@ -0,0 +1,29 @@
+library(readxl)
+library(tidyverse)
+
+desmoines.raw <- read_excel("Assoc. of WA Cities Signatories/Des Moines - closed/Data/Pursuit List.xlsx")
+
+desmoines.inci <- desmoines.raw %>%
+  mutate(inciID = Incident,
+         reason = Nature,
+         disposition = Disposition,
+         rtime = hms::as_hms(str_split_i(Reported, " ", 1)),
+         rdate = mdy(str_split_i(Reported, " ", 2)),
+         rdatetime = force_tz(rdate + lubridate::hms(rtime), tz="US/Pacific"),
+         mo = month(rdate),
+         da = day(rdate),
+         yr = year(rdate)
+         ) %>%
+  select(inciID:yr)
+
+demoines.notes <- list(format = "xls",
+                       uoa = "incident",
+                       limitations = "none known")
+
+save.image("Assoc. of WA Cities Signatories/Des Moines - closed/desmoines.rda")
+
+desmoines.inci %>%
+  group_by(yr, mo) %>%
+  count() %>%
+  ggplot(aes(x=factor(mo), y=n, group = yr, color=yr)) +
+  geom_line()
